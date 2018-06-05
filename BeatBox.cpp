7d4afeeb3979f0f0f2b692495e2f74e45ef35e74
CharlieMoreno <39964725+CharlieMoreno@users.noreply.github.com>
2018-06-05 22:33:23 +0200

    Transition from MPU6050 to MPU9250
    The code needed to get the accelerometer raw values have been modified to suit the MPU9250 library.

--- BeatBox.cpp
+++ BeatBox.cpp
@@ -24,7 +24,6 @@ RXD   <---> PIN 3
 */
 
 #include "BeatBox.h"
-#include "Accel.h"
 #include "Trigger.h"
 #include "Lights.h"
 #include "Taps.h"
@@ -33,18 +32,13 @@ RXD   <---> PIN 3
 #include "Play.h"
 #include "Log.h"
 #include "Debug.h"
-#include "I2Cdev.h"
-#include "MPU6050.h"
+//#include "I2Cdev.h"
+#include "SparkFunMPU9250-DMP.h"
 
 #define N 25
 
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 accelgyro;			// Declaración de clase tipo MPU6050 llamada accelgyro.
-//MPU6050 accelgyro(0x69);	// <-- use for AD0 high
+MPU9250_DMP accelgyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -53,57 +47,85 @@ Trigger accX;
 Trigger accY;
 Trigger accZ;
 
-void MPU6050setup() {								
-	// join I2C bus (I2Cdev library doesn't do this automatically)
-	Wire.begin();
-	
-	// initialize serial communication
-	// (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-	// it's really up to you depending on your project)
+
+
+unsigned long lastMillis = 0;
+unsigned long loopMillis = 0;
+unsigned long maxLoopMillis = 0;
+
+/////////////////////////////////////////////
+
+unsigned long triggerComparisonTime = 0;	//Tiempo que se deja tras un primer disparo para registrar los demÃ¡s posibles disparos. Cuando se acaba, se comparan los disparos y se escoge el de mayor valor absoluto.
+
+long lastMicros = micros();
+
+int counter = 0;	// Contador usado para numerar los resultados en el monitor serie.
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int axisTriggerValues[6] = {0, 0, 0, 0, 0, 0};	// getRealTrigger() guarda los valores de los disparos (yThreshold) en este vector [ejeX, ejeY, ejeZ] para despuÃ©s compararlos.
+
+float maxTriggerValue = 0;		// getRealTrigger() va a guardar en esta variable el mÃ¡ximo valor de los disparos de todos los ejes.
+
+int maxTriggerAxis = 0;			// Eje en el que se ha producido el impacto (mayor pico aceleraciÃ³n).
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MPU6050setup() {
+
 	Serial.begin(2000000);
 	
 	// initialize device
 	Serial.println("Initializing I2C devices...");
-	accelgyro.initialize();
 	
-	accelgyro.setXAccelOffset(-2194);
-	accelgyro.setYAccelOffset(-1871);
-	accelgyro.setZAccelOffset(868);
 	
-	accelgyro.setXGyroOffset(76);
-	accelgyro.setYGyroOffset(-6);
-	accelgyro.setZGyroOffset(37);
+	if (accelgyro.begin() != INV_SUCCESS)
+	{
+		while (1)
+		{
+			Serial.println("Unable to communicate with MPU-9250");
+			Serial.println("Check connections, and try again.");
+			Serial.println();
+			delay(5000);
+		}
+	}
 	
-	accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_16);
+	// Use setSensors to turn on or off MPU-9250 sensors.
+	// Any of the following defines can be combined:
+	// INV_XYZ_GYRO, INV_XYZ_ACCEL, INV_XYZ_COMPASS,
+	// INV_X_GYRO, INV_Y_GYRO, or INV_Z_GYRO
+	// Enable all sensors:
+	accelgyro.setSensors(INV_XYZ_ACCEL);
 
+	// Use setGyroFSR() and setAccelFSR() to configure the
+	// gyroscope and accelerometer full scale ranges.
+	// Gyro options are +/- 250, 500, 1000, or 2000 dps
+	// imu.setGyroFSR(2000); // Set gyro to 2000 dps
+	// Accel options are +/- 2, 4, 8, or 16 g
+	accelgyro.setAccelFSR(16); // Set accel to +/-16g
+	// Note: the MPU-9250's magnetometer FSR is set at
+	// +/- 4912 uT (micro-tesla's)
+
+	// setLPF() can be used to set the digital low-pass filter
+	// of the accelerometer and gyroscope.
+	// Can be any of the following: 188, 98, 42, 20, 10, 5
+	// (values are in Hz).
+	accelgyro.setLPF(5); // Set LPF corner frequency to 5Hz
+
+	// The sample rate of the accel/gyro can be set using
+	// setSampleRate. Acceptable values range from 4Hz to 1kHz
+	accelgyro.setSampleRate(1000); // Set sample rate to 1000Hz
+
+	// Likewise, the compass (magnetometer) sample rate can be
+	// set using the setCompassSampleRate() function.
+	// This value can range between: 1-100Hz
+	// imu.setCompassSampleRate(10); // Set mag rate to 10Hz
 	
-	// verify connection
-	Serial.println("Testing device connections...");
-	Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 }
 
 
-////MEDIA
-//float posBuffer[N];
-//float sum = 0;
-//
-//void posBufferSetup(){
-	//for(int j = 0; j<N; j++){
-		//posBuffer[j] = 0;
-	//}
-//}
-//
-//float updateAverage(float newValue){
-	//sum -= posBuffer[0];
-	//sum += newValue;
-	//for (int i=0; i < N-1; i++){
-		//posBuffer[i] = posBuffer[i+1];
-	//}
-	//posBuffer[N-1] = newValue;
-	//return sum/N;
-//}
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
 
@@ -118,113 +140,47 @@ void setup() {
 	logSetup();
 
 	MPU6050setup();		
-	
-	//initAccel();			// Esto era para el acelerómetro que usaba Yago.
 
-	for(int i = 0; i < 50; i++) {		//*******PARA QUÉ SE HACÍA ESTO 50 VECES?************** Para la media y el filtro tal vez? Calibración?	
-		accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);	
-		updateTrigger(&accX, ax);
-		updateTrigger(&accY, ay);
-		updateTrigger(&accZ, az);	
+	for(int i = 0; i < 50; i++) {		//*******PARA QUÃ‰ SE HACÃA ESTO 50 VECES?************** Para la media y el filtro tal vez? CalibraciÃ³n?	
+		accelgyro.updateAccel();	
+		updateTrigger(&accX, accelgyro.ax);
+		updateTrigger(&accY, accelgyro.ay);
+		updateTrigger(&accZ, accelgyro.az);	
 	}
 	
 	recordSetup();
 
-	//attachPeriodicInterrupt(updateAccel, 4); // Update Accel ~250 times per second.		// Esto era para el acelerómetro que usaba Yago.
+	//attachPeriodicInterrupt(updateAccel, 4); // Update Accel ~250 times per second.		// Esto era para el acelerÃ³metro que usaba Yago.
 	
 	//posBufferSetup();
 
 }
 
-
-//char strBuff[64];
-//void plotAcc(int a) {
-	//for(int i = 0; i < 63; i++) {
-		//strBuff[i] = ' ';
-		//if (i == (a >> 5)) strBuff[i] = 'x';
-	//}
-	//strBuff[63] = 0;
-	//Serial.println(strBuff);
-//}
-
-unsigned long lastMillis = 0;
-unsigned long loopMillis = 0;
-unsigned long maxLoopMillis = 0;
-
-//long int timeIn;
-//int i;	// Para qué demonios es esta i??
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-/////////YA NO UTILIZO EL MODELO DEL MUELLE
-//float pos = 50, vel = 0, aTot = 0, aMuelle = 0, aVisc = 0;
-//float posx, velx;
-//float len = 100, kMuelle = 200, rozamiento = 0.05, t = 0.03;
-//int golpe = 0;
-//
-//int percute(int a) {
-	//aMuelle = (pos - len/2)*kMuelle;
-	//aVisc = (vel > 0 ? -rozamiento * vel * vel : (vel < 0 ? rozamiento * vel * vel : 0));
-	//aTot = a - aMuelle + aVisc;
-	//vel += aTot * t;
-	////if (abs(vel) > 0)  
-	//pos += vel * t + aTot/2 * t * t ;
-	////Condiciones de contorno:
-	//if (pos >= len ) {
-		//pos = len;
-		//vel = 0;
-		//golpe += 4000;
-		////return pos*100;
-	//}
-	//if (pos <= 0) {
-		//pos = 0;
-		//vel = 0;
-		//golpe -= 4000;
-		////return -pos*100;
-	//}
-	//if (az > 15000) {
-		//kMuelle += 5;
-	//}
-	//return pos*100;
-//}
 
-/////////////////////////////////////////////
-
-
-unsigned long triggerComparisonTime = 0;	//Tiempo que se deja tras un primer disparo para registrar los demás posibles disparos. Cuando se acaba, se comparan los disparos y se escoge el de mayor valor absoluto.
-
-long lastMicros = micros();
-	
-int counter = 0;	// Contador usado para numerar los resultados en el monitor serie.
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int axisTriggerValues[6] = {0, 0, 0, 0, 0, 0};	// getRealTrigger() guarda los valores de los disparos (yThreshold) en este vector [ejeX, ejeY, ejeZ] para después compararlos.
-
-float maxTriggerValue = 0;		// getRealTrigger() va a guardar en esta variable el máximo valor de los disparos de todos los ejes.
-
-int maxTriggerAxis = 0;			// Eje en el que se ha producido el impacto (mayor pico aceleración).
 
 
 void loop() {
 	
 	
-	//i = 0;	//Para qué demonios es esta i, declarada más arriba?
+	//i = 0;	//Para quÃ© demonios es esta i, declarada mÃ¡s arriba?
 	
-	accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	accelgyro.updateAccel();
 
-	updateTrigger(&accX, ax);
-	updateTrigger(&accY, ay);
-	updateTrigger(&accZ, az);
+	updateTrigger(&accX, accelgyro.ax);
+	updateTrigger(&accY, accelgyro.ay);
+	updateTrigger(&accZ, accelgyro.az);
 	
 	/*Serial.print(getRealTrigger(&accX, &accY, &accZ, v));		//ESTO VA BIEN
 	Serial.print("\t");*/
 
 
-	int maxTriggerAxis = getRealTrigger(&accX, &accY, &accZ, axisTriggerValues, &maxTriggerValue, &counter);		// Devuelve el eje en el que se ha producido el impacto (mayor pico aceleración) y guarda en maxTriggerValue el valor de ese impacto, ó devuelve -1 mientras no haya impacto.
+	int maxTriggerAxis = getRealTrigger(&accX, &accY, &accZ, axisTriggerValues, &maxTriggerValue, &counter);		// Devuelve el eje en el que se ha producido el impacto (mayor pico aceleraciÃ³n) y guarda en maxTriggerValue el valor de ese impacto, Ã³ devuelve -1 mientras no haya impacto.
 
-	// Comprobación de acierto por monitor serie:
-	if (maxTriggerAxis != -1){	// Si se ha producido un impacto, escribe el número del impacto, el eje y la magnitud de los impactos en los 6 ejes:
+	// ComprobaciÃ³n de acierto por monitor serie:
+	if (maxTriggerAxis != -1){	// Si se ha producido un impacto, escribe el nÃºmero del impacto, el eje y la magnitud de los impactos en los 6 ejes:
 
 		if (maxTriggerAxis == 0) { Serial.print(++counter); Serial.print("\t"); Serial.print(getCurrentL(&accX)); Serial.println("\tX+"); }
 		if (maxTriggerAxis == 1) { Serial.print(++counter); Serial.print("\t"); Serial.print(getCurrentR(&accX)); Serial.println("\tX-"); }
@@ -250,7 +206,7 @@ void loop() {
 			
 			
 			
-	//Cuánto dura la vuelta de loop más larga?:
+	//CuÃ¡nto dura la vuelta de loop mÃ¡s larga?:
 	loopMillis = millis() - lastMillis;
 	maxLoopMillis = max(maxLoopMillis, loopMillis);
 	lastMillis = millis();
@@ -258,6 +214,13 @@ void loop() {
 	//debugUpdate();
 	
 }
+
+
+
+
+
+
+////////// FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////
 
 
 
