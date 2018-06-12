f2191f5b6bff733cc1cd8c5dc5c9b56b854b3fcf
CharlieMoreno <charlietalcual@gmail.com>
2018-06-12 20:36:39 +0200

    Added AtmelStudio project to 'AtmelStudio' folder
    In order to organize.

---
+++ AtmelStudio/GccApplication1/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+BeatBox.cpp
+
+Debug.cpp
+
+Lights.cpp
+
+Log.cpp
+
+Mean.cpp
+
+Midi.cpp
+
+Play.cpp
+
+Record.cpp
+
+Taps.cpp
+
+Trigger.cpp
+
